807a8673d05229853b3b064cd0260fb4c4a31cab
Christian Weyer <christian.weyer@thinktecture.com>
2022-05-21 16:16:14 +0200

    Initial code commit

---
+++ ClientDaprized/Program.cs
@@ -0,0 +1,40 @@
+﻿using Dapr.Client;
+using DaprData;
+using System.Net.Http.Json;
+
+Console.WriteLine("Hello, Dapr!");
+
+using var client = new DaprClientBuilder().Build();
+
+// Feature: service invocation - with Dapr SDK
+var result = client.CreateInvokeMethodRequest(
+  HttpMethod.Get, 
+  "service-daprized", 
+  "weatherforecast");
+
+await client.InvokeMethodAsync(result);
+Console.WriteLine("*** Dapr: SDK invoke result: " + result);
+
+
+// Feature: service invocation - with Dapr HttpClient
+var httpClient = DaprClient.CreateInvokeHttpClient();
+
+var weatherForecasts =
+    await httpClient.GetFromJsonAsync<List<WeatherForecast>>(
+        "http://service-daprized/weatherforecast");
+Console.WriteLine("*** Dapr: HttpClient invoke result: " + result);
+
+
+// Feature: output binding
+var metadata = new Dictionary<string, string> {
+    {"emailFrom", "christian.weyer@thinktecture.com"},
+    {"emailTo", "christian.weyer@thinktecture.com"},
+    {"subject", "An email from Dapr SendGrid binding"}
+  };
+
+var body =  "<h1>Hello, Techorama!</h1>Greetings.<br>Bye!";
+
+await client.InvokeBindingAsync("sendgrid", "create", body, metadata);
+Console.WriteLine("*** Dapr: Email sent!");
+
+Console.ReadLine();
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# dapr-basics-demos
+# Dapr basics demos
 Simple demos of Dapr's features.
+
+## Start applications (self-hosted)
+```bash
+    cd ServiceDaprized
+	dapr run --app-id service-daprized --app-port 6001 --dapr-http-port 3601 --dapr-grpc-port 60001 dotnet run
+```
+
+```bash
+    cd ClientDaprized
+	dapr run --app-id client-daprized --components-path ./../components dotnet run
+```
