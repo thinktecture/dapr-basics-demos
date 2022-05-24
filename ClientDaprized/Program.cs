f432f0abeda608f0eb7fdd14a628447ee60e20ed
Christian Weyer <christian.weyer@thinktecture.com>
2022-05-24 18:29:02 +0200

    Fixed resiliency config

--- ClientDaprized/Program.cs
+++ ClientDaprized/Program.cs
@@ -1,12 +1,14 @@
 ﻿using Dapr.Client;
 using DaprData;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 Console.WriteLine("Hello, Dapr!");
 
 using var client = new DaprClientBuilder().Build();
 
 // Feature: service invocation - with Dapr SDK
+/*
 var request = client.CreateInvokeMethodRequest(
   HttpMethod.Get, 
   "service-daprized", 
@@ -14,7 +16,7 @@ var request = client.CreateInvokeMethodRequest(
 
 var response = await client.InvokeMethodWithResponseAsync(request);
 Console.WriteLine("*** Dapr: SDK invoke result: " + response.Content.ReadAsStringAsync().Result);
-
+*/
 
 // Feature: service invocation - with Dapr HttpClient
 var httpClient = DaprClient.CreateInvokeHttpClient();
@@ -22,10 +24,10 @@ var httpClient = DaprClient.CreateInvokeHttpClient();
 var weatherForecasts =
     await httpClient.GetFromJsonAsync<List<WeatherForecast>>(
         "http://service-daprized/weatherforecast");
-Console.WriteLine("*** Dapr: HttpClient invoke result: " + weatherForecasts);
-
+Console.WriteLine("*** Dapr: HttpClient invoke result: " + JsonSerializer.Serialize(weatherForecasts));
 
 // Feature: output binding
+/*
 var metadata = new Dictionary<string, string> {
     {"emailFrom", "christian.weyer@thinktecture.com"},
     {"emailTo", "christian.weyer@thinktecture.com"},
@@ -36,6 +38,6 @@ var body =  "<h1>Hello, Techorama!</h1>Greetings.<br>Bye!";
 
 await client.InvokeBindingAsync("sendgrid", "create", body, metadata);
 Console.WriteLine("*** Dapr: Email sent!");
-
+*/
 
 Console.ReadLine();
--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Simple demos of Dapr's features.
 ## Start applications (self-hosted)
 ```bash
 cd ServiceDaprized
-dapr run --enable-api-logging --app-id service-daprized --app-port 6001 --dapr-http-port 3601 --dapr-grpc-port 60001 dotnet run
+dapr run --app-id service-daprized --app-port 6001 --dapr-http-port 3601 --dapr-grpc-port 60001 --enable-api-logging dotnet run
 ```
 
 ```bash
 cd ClientDaprized
-dapr run --enable-api-logging --app-id client-daprized --components-path ./../components dotnet run
+dapr run --app-id client-daprized --config ../configuration/config.yaml --components-path ./../components --enable-api-logging dotnet run
 ```
 
 ## Dapr Dashboard
