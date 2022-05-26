0595bcc0d696dcd2f9be6a320d6d45edcb99ad46
Christian Weyer <christian.weyer@thinktecture.com>
2022-05-26 22:48:32 +0200

    Added console menu

--- ClientDaprized/Program.cs
+++ ClientDaprized/Program.cs
@@ -1,55 +1,86 @@
-﻿using Dapr.Client;
+﻿using ConsoleTools;
+using Dapr.Client;
 using DaprData;
 using System.Net.Http.Json;
 using System.Text.Json;
 
-Console.WriteLine("Hello, Dapr!");
+public partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Dapr!");
 
-using var client = new DaprClientBuilder().Build();
+        var menu = new ConsoleMenu(args, level: 0)
+          .Add("Service invocation SDK client", async () => await InvokeServiceWithDaprSdkClient())
+          .Add("Service invocation Http client", async () => await InvokeServiceWithDaprHttpClient())
+          .Add("Output binding", async () => await InvokeOutputBinding())
+          .Add("Publish / subscribe", async () => await PublishEvent())
+          .Add("Exit", () => Environment.Exit(0))
+          .Configure(config =>
+          {
+              config.Selector = "--> ";
+              config.Title = "Dapr demos";
+              config.EnableBreadcrumb = true;
+          });
 
-// Feature: service invocation - with Dapr SDK
-/*
-var request = client.CreateInvokeMethodRequest(
-  HttpMethod.Get, 
-  "service-daprized", 
-  "weatherforecast");
+        menu.Show();
+    }
 
-var response = await client.InvokeMethodWithResponseAsync(request);
-Console.WriteLine("*** Dapr: SDK invoke result: " + response.Content.ReadAsStringAsync().Result);
-*/
+    // Feature: service invocation - with Dapr SDK
+    private static async Task InvokeServiceWithDaprSdkClient()
+    {
+        using var client = new DaprClientBuilder().Build();
 
+        var request = client.CreateInvokeMethodRequest(
+          HttpMethod.Get,
+          "service-daprized",
+          "weatherforecast");
 
-// Feature: service invocation - with Dapr HttpClient
-/*
-var httpClient = DaprClient.CreateInvokeHttpClient();
+        var response = await client.InvokeMethodWithResponseAsync(request);
 
-var weatherForecasts =
-    await httpClient.GetFromJsonAsync<List<WeatherForecast>>(
-        "http://service-daprized/weatherforecast");
-Console.WriteLine("*** Dapr: HttpClient invoke result: " + JsonSerializer.Serialize(weatherForecasts));
-*/
+        Console.WriteLine("*** Dapr: SDK invoke result: " + response.Content.ReadAsStringAsync().Result);
+    }
 
+    // Feature: service invocation - with Dapr HttpClient
+    private static async Task InvokeServiceWithDaprHttpClient()
+    {
+        var httpClient = DaprClient.CreateInvokeHttpClient();
 
-// Feature: output binding
-/*
-var metadata = new Dictionary<string, string> {
-    {"emailFrom", "christian.weyer@thinktecture.com"},
-    {"emailTo", "christian.weyer@thinktecture.com"},
-    {"subject", "An email from Dapr SendGrid binding"}
-  };
+        var weatherForecasts =
+            await httpClient.GetFromJsonAsync<List<WeatherForecast>>(
+                "http://service-daprized/weatherforecast");
 
-var body =  "<h1>Hello, you!</h1>Greetings.<br>Bye!";
+        Console.WriteLine("*** Dapr: HttpClient invoke result: " + JsonSerializer.Serialize(weatherForecasts));
+    }
 
-await client.InvokeBindingAsync("sendgrid", "create", body, metadata);
-Console.WriteLine("*** Dapr: Email sent!");
-*/
+    // Feature: output binding
+    private static async Task InvokeOutputBinding()
+    {
+        using var client = new DaprClientBuilder().Build();
 
+        var metadata = new Dictionary<string, string> {
+          {"emailFrom", "christian.weyer@thinktecture.com"},
+          {"emailTo", "christian.weyer@thinktecture.com"},
+          {"subject", "An email from Dapr SendGrid binding"}
+        };
 
-// Feature: pub-sub
-var order = new Order(42);
+        var body = "<h1>Hello, you!</h1>Greetings.<br>Bye!";
 
-await client.PublishEventAsync("pubsub", "orders", order);
-    Console.WriteLine("Published data: " + order);
-Console.WriteLine("*** Dapr: Order submitted!");
+        await client.InvokeBindingAsync("sendgrid", "create", body, metadata);
 
-Console.ReadLine();
+        Console.WriteLine("*** Dapr: Email sent!");
+    }
+
+    // Feature: pub-sub
+    private static async Task PublishEvent()
+    {
+        using var client = new DaprClientBuilder().Build();
+
+        var order = new Order(42);
+
+        await client.PublishEventAsync("pubsub", "orders", order);
+
+        Console.WriteLine("Published data: " + order);
+        Console.WriteLine("*** Dapr: Order submitted!");
+    }
+}
